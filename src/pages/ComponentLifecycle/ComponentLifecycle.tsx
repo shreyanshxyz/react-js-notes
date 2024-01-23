d7032ad45f580c21b11cef8044e283306ccf62b1
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-23 15:44:09 +0530

    update ComponentLifecycle.tsx

--- src/pages/ComponentLifecycle/ComponentLifecycle.tsx
+++ src/pages/ComponentLifecycle/ComponentLifecycle.tsx
@@ -7,6 +7,8 @@ const ComponentLifecycle: React.FC<ComponentLifecycleProps> = () => {
   return (
     <div className="blog__container">
       <h1 className="blog__heading"> Component Lifecycle (Deprecated)</h1>
+      <h4 className="blog__subheading">Mounting</h4>
+      <div className="blog__text">TBD</div>
     </div>
   );
 };
