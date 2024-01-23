94bebd5c73b69ad19328ebc6e251e74874652f65
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-23 15:47:41 +0530

    feat: create Routing.tsx

---
+++ src/pages/Routing/Routing.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type RoutingProps = {};
+
+const Routing: React.FC<RoutingProps> = () => {
+  return <div className="blog__heading">Have a good coding</div>;
+};
+export default Routing;
